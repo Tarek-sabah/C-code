4a3ccd1fc0544f9a9d04602e335adac7585bbbe3
Tarek <tareksabah208@gmail.com>
2023-06-06 07:05:17 +0300

    Modified Simple Calculator to use floating numbers

--- 002_Simple Calculator/main.c
+++ 002_Simple Calculator/main.c
@@ -1,27 +1,28 @@
 #include "stdio.h"
 
-int Calculate(int *Ptr_num1,int *Ptr_num2, char *Ptr_operand);
+float Calculate(float *Ptr_num1,float *Ptr_num2, char *Ptr_operand);
 
 int main()
 {
-    int Local_num1,Local_num2,Local_result;
+    float Local_num1,Local_num2;
+    float Local_result;
     char Local_operand;
     printf("Enter First Number: ");
-    scanf("%d",&Local_num1);
+    scanf("%f",&Local_num1);
 
     printf("Enter Second Number: ");
-    scanf("%d",&Local_num2);
+    scanf("%f",&Local_num2);
 
     printf("Enter Operand: ");
     scanf(" %c",&Local_operand);
 
     Local_result = Calculate(&Local_num1,&Local_num2,&Local_operand);
  
-    printf("Result = %d",Local_result);
+    printf("Result = %.2f",Local_result);
     return 0;
 }
 
-int Calculate(int *Ptr_num1,int *Ptr_num2, char *Ptr_operand)
+float Calculate(float *Ptr_num1,float *Ptr_num2, char *Ptr_operand)
 {
     switch(*Ptr_operand)
     {
@@ -29,7 +30,7 @@ int Calculate(int *Ptr_num1,int *Ptr_num2, char *Ptr_operand)
         case '-':return ((*Ptr_num1) - (*Ptr_num2));break;
         case '*':return ((*Ptr_num1) * (*Ptr_num2));break;
         case '/':return ((*Ptr_num1) / (*Ptr_num2));break;
-        case '%':return ((*Ptr_num1) % (*Ptr_num2));break;
+        case '%':return (((int)*Ptr_num1) % ((int)*Ptr_num2));break;
         default: printf("Operator is wrong");
     }
 }
